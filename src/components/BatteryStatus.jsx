fea91fbe114551693099761c7a4db56a263639bf
Kumud Gupta <40192898+kumudgupta76@users.noreply.github.com>
2024-09-15 13:16:22 +0530

    Notification feature

--- src/components/BatteryStatus.jsx
+++ src/components/BatteryStatus.jsx
@@ -1,4 +1,4 @@
-import { Col, Row, Typography, Progress, Spin } from 'antd';
+import { Col, Row, Typography, Progress, Spin, notification, Input, Button } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
 
@@ -9,22 +9,44 @@ const BatteryStatus = () => {
   const [value, setValue] = useState(0);
   const [batterySupported, setBatterySupported] = useState(true);
   const [batteryCharging, setBatteryCharging] = useState(false);
+  const [notificationClosed, setNotificationClosed] = useState(false);
+  const [testValue, setTestValue] = useState(100);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       // Call your method here
-      myMethod();
+    myMethod();
     }, 1000); // Interval of 1000 milliseconds (1 seconds)
 
     // Clear the interval when the component unmounts
     return () => clearInterval(intervalId);
   }, []); // Empty dependency array to run this effect only once when the component mounts
 
+  const showNotification = (percentage) => {  
+    Notification.requestPermission().then(perm => {
+      console.log(perm);
+      if (perm === 'granted' && (percentage < 20)) {
+
+        console.log('Notification granted');
+        const notificationjs = new Notification('Battery Low ⚠️', {
+          body: `Battery is ${percentage}%`,
+          icon: '/icon.png',
+          requireInteraction: true
+        });
+
+        notificationjs.onclick = () => {
+          notificationjs.close();
+          setNotificationClosed(true);
+        };
+
+        notificationjs.onclose = () => {
+          setNotificationClosed(true);
+        };
+      }
+    });
+  };
   // Method to be called at the interval
   const myMethod = () => {
-    console.log('Method called every 1 second');
-    // Add your method logic here
-
     if ('getBattery' in navigator) {
       navigator.getBattery().then(function (battery) {
         // Update battery status initially
@@ -44,6 +66,7 @@ const BatteryStatus = () => {
         var percentage = Math.round(battery.level * 100);
         setValue(percentage);
         setBatteryCharging(battery.charging);
+        showNotification(percentage);
       }
     } else {
       setBatterySupported(false);
@@ -61,18 +84,19 @@ const BatteryStatus = () => {
       '0%': '#ff4d4f',
       '100%': '#f47340'
     },
-    'green' :{
+    'green': {
       '0%': '#87d068',
       '100%': '#52c41a',
     },
-    'blue':{
+    'blue': {
       '0%': '#1890ff',
       '100%': '#74bcff',
-    }  }
+    }
+  }
 
   const getBatteryColor = (value) => {
     if (value < 20) {
-      return 'red'; 
+      return 'red';
     } else if (value <= 70) {
       return 'blue';
     } else {
@@ -101,13 +125,14 @@ const BatteryStatus = () => {
               <Progress type='dashboard' percent={value} strokeColor={gradientColorsMap[getBatteryColor(value)]} size='large' />
             </Col>
             <Col span={24} style={{ textAlign: 'center', marginTop: '30px' }}>
-              <Progress steps={5} percent={value} strokeWidth={30}  strokeColor={colorMap[getBatteryColor(value)]}/>
-
+              <Progress steps={5} percent={value} strokeWidth={30} strokeColor={colorMap[getBatteryColor(value)]} />
             </Col>
             {batteryCharging && <Col span={24} style={{ textAlign: 'center', justifyContent: "center", marginTop: '30px' }}>
               <Title level={4}>Charging⚡</Title>
             </Col>}
+            <div><Button onClick={(e) => showNotification(12)}>Test Notification</Button></div>
           </Row>
+          
           : <Title level={3}> Battery Status API not supported</Title>
       }
     </div>
